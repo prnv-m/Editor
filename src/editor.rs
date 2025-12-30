1f61224924793a3a5f1183b5efce8fb86945569b
root <root@Pranav.>
2025-12-30 14:46:59 +0000

    Basic shell editor

---
+++ src/editor.rs
@@ -0,0 +1,37 @@
+use crossterm::event::{read, Event::Key, KeyCode::Char};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use std::io::{self, Read};
+
+
+
+pub struct Editor{
+    
+}
+
+impl Editor{
+    pub fn default() -> Self{
+        Editor{}
+    }
+
+    pb fn run(&self){
+        enable_raw_mode().unwrap();
+        for b in io::stdin().bytes(){
+            let b = b.unwrap();
+            let c = b as char;
+
+            if c.is_control(){
+                println!("Binary: {0:08b} ASCII: {0:#
+                03} \r", b);
+
+            }else{
+                println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+            }
+            if c == 'q'{
+                disable_raw_mode().unwrap();
+                break;
+            }
+        }
+    }
+
+    
+}
